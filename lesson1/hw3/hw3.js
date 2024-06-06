66d8df3abe8f9f229baa91bf1b937d0ad0792469
NadiyPro <siroviyn13@gmail.com>
2024-06-06 13:53:52 +0300

    create

---
+++ lesson1/hw3/hw3.js
@@ -0,0 +1,29 @@
+// // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+var TextT = /** @class */ (function () {
+    function TextT(text) {
+        this.text = text;
+    }
+    TextT.prototype.textFunction = function () {
+        for (var i = 0; i < 10; i++) {
+            var blocks = this[i];
+            var div = document.createElement('div');
+            div.innerHTML = "<p>".concat(blocks, "</p>");
+            document.body.appendChild(div);
+        }
+        return;
+    };
+    return TextT;
+}());
+var text = [
+    new TextT('Lorem ipsum dolor sit amet.'),
+    new TextT('Lorem ipsum dolor sit amet.'),
+    new TextT('Lorem ipsum dolor sit amet.'),
+    new TextT('Lorem ipsum dolor sit amet.'),
+    new TextT('Lorem ipsum dolor sit amet.'),
+    new TextT('Lorem ipsum dolor sit amet.'),
+    new TextT('Lorem ipsum dolor sit amet.'),
+    new TextT('Lorem ipsum dolor sit amet.'),
+    new TextT('Lorem ipsum dolor sit amet.'),
+    new TextT('Lorem ipsum dolor sit amet.')
+];
+text.forEach(function (value) { value.textFunction(); });
