747a3236e816f786629c4f1c8a639ca4db13bf48
NadiyPro <siroviyn13@gmail.com>
2024-06-06 17:22:54 +0300

    create

--- lesson1/hw3/hw3.js
+++ lesson1/hw3/hw3.js
@@ -28,3 +28,37 @@ text.forEach(function (value) { value.textFunction(); });
 //     let blocks:TextT=new TextT (value.text)
 //     blocks.textFunction()
 // });
+// // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+var TextIndex = /** @class */ (function () {
+    function TextIndex(textIndex) {
+        this.textIndex = textIndex;
+    }
+    TextIndex.prototype.textIndexFunction = function () {
+        var divIndex = document.createElement('div');
+        // divIndex.innerHTML=this.textIndex;
+        divIndex.textContent = this.textIndex;
+        document.body.appendChild(divIndex);
+        return;
+    };
+    return TextIndex;
+}());
+var textIndex = [
+    new TextIndex('Lorem ipsum dolor sit amet.'),
+    new TextIndex('Lorem ipsum dolor sit amet.'),
+    new TextIndex('Lorem ipsum dolor sit amet.'),
+    new TextIndex('Lorem ipsum dolor sit amet.'),
+    new TextIndex('Lorem ipsum dolor sit amet.'),
+    new TextIndex('Lorem ipsum dolor sit amet.'),
+    new TextIndex('Lorem ipsum dolor sit amet.'),
+    new TextIndex('Lorem ipsum dolor sit amet.'),
+    new TextIndex('Lorem ipsum dolor sit amet.'),
+    new TextIndex('Lorem ipsum dolor sit amet.')
+];
+for (var i = 0; i < 10; i++) {
+    var finishText = new TextIndex("".concat(i + 1, " ").concat(textIndex[i]));
+    finishText.textIndexFunction();
+}
+// for (let i:number=0;i<10;i++){
+//     let finishText:TextIndex = new TextIndex(`${i+1} ${textIndex[i]}`)
+//     finishText.textIndexFunction();
+// }
--- lesson1/hw3/hw3.ts
+++ lesson1/hw3/hw3.ts
@@ -28,4 +28,37 @@ text.forEach(value=> {value.textFunction()});
 //     blocks.textFunction()
 // });
 
+// // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+class TextIndex {
+    constructor(public textIndex:string) {
+    }
+    textIndexFunction():string{
+        let divIndex:HTMLElement = document.createElement('div');
+        // divIndex.innerHTML=this.textIndex;
+        divIndex.textContent=this.textIndex;
+        document.body.appendChild(divIndex)
+        return;
+    }
+}
 
+let textIndex: TextIndex[]= [
+    new TextIndex ('Lorem ipsum dolor sit amet.'),
+    new TextIndex ('Lorem ipsum dolor sit amet.'),
+    new TextIndex ('Lorem ipsum dolor sit amet.'),
+    new TextIndex ('Lorem ipsum dolor sit amet.'),
+    new TextIndex ('Lorem ipsum dolor sit amet.'),
+    new TextIndex ('Lorem ipsum dolor sit amet.'),
+    new TextIndex ('Lorem ipsum dolor sit amet.'),
+    new TextIndex ('Lorem ipsum dolor sit amet.'),
+    new TextIndex ('Lorem ipsum dolor sit amet.'),
+    new TextIndex ('Lorem ipsum dolor sit amet.')
+];
+for (let i:number=0;i<10;i++){
+    let finishText:TextIndex = new TextIndex(`${i+1} ${textIndex[i]}`)
+    finishText.textIndexFunction();
+}
+
+// for (let i:number=0;i<10;i++){
+//     let finishText:TextIndex = new TextIndex(`${i+1} ${textIndex[i]}`)
+//     finishText.textIndexFunction();
+// }
