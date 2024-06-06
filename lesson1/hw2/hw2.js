c81d94c4cf2d9f22cd7271e8350d96552c5144f6
NadiyPro <siroviyn13@gmail.com>
2024-06-06 12:09:09 +0300

    create

--- lesson1/hw2/hw2.js
+++ lesson1/hw2/hw2.js
@@ -307,3 +307,78 @@ function num(number2_1, number2_2) {
 var number2_1 = +prompt('Enter a number 1');
 var number2_2 = +prompt('Enter a number 2');
 num(number2_1, number2_2);
+// // - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
+// //     буде присвоювати змінній х значення "default"  якщо ви намагаєтесь присвоїти в неї falsy-значення (хибноподібні, тобто ті, які приводиться до false, а це 0 null undefined і тд).
+// // варіант рішення 1
+var xx = false;
+if (!xx) {
+    console.log('default');
+}
+// // варіант рішення 2
+var xxx = false;
+xxx = xxx || 'default';
+console.log(xxx);
+// // - з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray.
+// //     За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
+var Course = /** @class */ (function () {
+    function Course(title, monthDuration) {
+        this.title = title;
+        this.monthDuration = monthDuration;
+    }
+    Course.prototype.checkDuration = function () {
+        if (this.monthDuration >= 5) {
+            console.log('Супер');
+        }
+        else {
+            console.log('Погано');
+        }
+    };
+    return Course;
+}());
+var coursesAndDurationArray = [
+    new Course('JavaScript Complex', 5),
+    new Course('Java Complex', 6),
+    new Course('Python Complex', 6),
+    new Course('QA Complex', 4),
+    new Course('FullStack', 7),
+    new Course('Frontend', 4)
+];
+coursesAndDurationArray.forEach(function (course) { return course.checkDuration(); });
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// if (coursesAndDurationArray[0].monthDuration >= 5){
+//     console.log('Супер');
+// } else {
+//     console.log('Погано')
+// }
+// if (coursesAndDurationArray[1].monthDuration >= 5){
+//     console.log('Супер');
+// } else {
+//     console.log('Погано')
+// }
+// if (coursesAndDurationArray[2].monthDuration >= 5){
+//     console.log('Супер');
+// } else {
+//     console.log('Погано')
+// }
+// if (coursesAndDurationArray[3].monthDuration >= 5){
+//     console.log('Супер');
+// } else {
+//     console.log('Погано')
+// }
+// if (coursesAndDurationArray[4].monthDuration >= 5){
+//     console.log('Супер');
+// } else {
+//     console.log('Погано')
+// }
+// if (coursesAndDurationArray[5].monthDuration >= 5){
+//     console.log('Супер');
+// } else {
+//     console.log('Погано')
+// }
--- lesson1/hw2/hw2.ts
+++ lesson1/hw2/hw2.ts
@@ -331,7 +331,6 @@ console.log(xxx);
 // //     За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
 class Course {
 
-
     constructor(public title:string,public monthDuration:number ) {
 
     }
@@ -352,43 +351,5 @@ let coursesAndDurationArray: Course[] = [
     new Course('FullStack', 7),
     new Course('Frontend', 4)
 ];
+coursesAndDurationArray.forEach(course => course.checkDuration());
 
-
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
-// if (coursesAndDurationArray[0].monthDuration >= 5){
-//     console.log('Супер');
-// } else {
-//     console.log('Погано')
-// }
-// if (coursesAndDurationArray[1].monthDuration >= 5){
-//     console.log('Супер');
-// } else {
-//     console.log('Погано')
-// }
-// if (coursesAndDurationArray[2].monthDuration >= 5){
-//     console.log('Супер');
-// } else {
-//     console.log('Погано')
-// }
-// if (coursesAndDurationArray[3].monthDuration >= 5){
-//     console.log('Супер');
-// } else {
-//     console.log('Погано')
-// }
-// if (coursesAndDurationArray[4].monthDuration >= 5){
-//     console.log('Супер');
-// } else {
-//     console.log('Погано')
-// }
-// if (coursesAndDurationArray[5].monthDuration >= 5){
-//     console.log('Супер');
-// } else {
-//     console.log('Погано')
-// }
