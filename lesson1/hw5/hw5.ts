b79f1e31e7e31a002803c817e2ccf2f91c369e1f
NadiyPro <siroviyn13@gmail.com>
2024-06-09 17:23:37 +0300

    create

--- lesson1/hw5/hw5.ts
+++ lesson1/hw5/hw5.ts
@@ -34,3 +34,15 @@ function iterator (objectValue:Obj[]) {
     document.body.appendChild(divBasic);
 }
 iterator(objectItems);
+// // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
+type Sum = number
+
+let sumArr:Sum[] = [1,2,10,13,5];
+let sum = (valueSum:Sum[])=> {
+    let sumResult:number = 0;
+    for(let i:number=0;i<valueSum.length;i++){
+        sumResult += valueSum[i];
+    }
+    return console.log (sumResult);
+}
+sum (sumArr);
